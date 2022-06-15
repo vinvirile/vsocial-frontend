ce0b6934f82a995b77249564c0367b64b072d24a
cagycee <71255514+cagycee@users.noreply.github.com>
2022-06-15 17:30:01 -0500

    Site Structure Update

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -2,4 +2,5 @@ const sendStatus = document.querySelector('.statusform__send')
 
 sendStatus.addEventListener('click', () => {
   alert('Status sent')
+  document.querySelector('.statusform__field').value = ''
 })
