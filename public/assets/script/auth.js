6da3db238ac92259988ea6be783ff425690b2464
cagycee <71255514+cagycee@users.noreply.github.com>
2022-07-05 20:06:51 -0500

    Site Structure Update

--- public/assets/script/auth.js
+++ public/assets/script/auth.js
@@ -1,28 +1,60 @@
-const form = document.querySelector('form')
+// GRABBING DOM ELEMENTS
+const formSignIn = document.querySelector('.signinform') || ''
+const formSignUp = document.querySelector('.signupform') || ''
+const formEmail = document.querySelector('.form__email')
 const formUsername = document.querySelector('.form__username')
 const formPassword = document.querySelector('.form__password')
-const demoAccount = document.querySelector('.loginbox__form__demoaccount')
+const formRePassword = document.querySelector('.form__repassword')
+const demoAccount = document.querySelector('.loginbox__form__demoaccount') || ''
 const signinBtn = document.querySelector('.loginbox__form__signinbtn')
 const err = document.querySelector('.loginbox__form__err')
 
-demoAccount.addEventListener('click', () => {
-  formUsername.value = 'johndoe'
-  formPassword.value = 'johndoe'
-})
+if (demoAccount !== '') {
+  demoAccount.addEventListener('click', () => {
+    formUsername.value = 'johndoe'
+    formPassword.value = 'johndoe'
+  })
+}
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+if (formSignIn !== '') {
+  formSignIn.addEventListener('submit', e => {
+    e.preventDefault()
 
-  err.textContent = ''
-  signinBtn.innerHTML =
-    '<div class="loader"><div></div><div></div><div></div><div></div></div>'
+    err.textContent = ''
+    signinBtn.innerHTML =
+      '<div class="loader"><div></div><div></div><div></div><div></div></div>'
 
-  setTimeout(() => {
-    if (formUsername.value === '' || formPassword.value === '') {
-      signinBtn.textContent = 'Sign in'
-      err.textContent = 'All fields are required!'
-    } else {
+    setTimeout(() => {
+      if (formUsername.value === '' || formPassword.value === '') {
+        signinBtn.textContent = 'Sign in'
+        err.textContent = 'All fields are required!'
+        return
+      }
       window.location.href = 'index.html'
-    }
-  }, 2000)
-})
+    }, 2000)
+  })
+}
+
+if (formSignUp !== '') {
+  formSignUp.addEventListener('submit', e => {
+    e.preventDefault()
+
+    err.textContent = ''
+    signinBtn.innerHTML =
+      '<div class="loader"><div></div><div></div><div></div><div></div></div>'
+
+    setTimeout(() => {
+      if (
+        formEmail.value === '' ||
+        formUsername === '' ||
+        formPassword === '' ||
+        formRePassword === ''
+      ) {
+        signinBtn.textContent = 'Sign in'
+        err.textContent = 'All fields are required!'
+        return
+      }
+      window.location.href = 'index.html'
+    }, 3000)
+  })
+}
